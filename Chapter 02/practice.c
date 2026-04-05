c0467bb0eb0e83abe35e67b6f5b29baf4b669216
Wasiq-Ali <wasiqali878@gmail.com>
2026-04-06 01:56:31 +0500

    practice: Done chapter 02

---
+++ Chapter 02/practice.c
@@ -0,0 +1,61 @@
+// Question One is Done: Write a program to print the average of 3 numbers.
+
+// #include <stdio.h>
+
+// int main() {
+// 	int number_1 = 5;
+// 	int number_2 = 7;
+// 	float number_3 = 18.5;
+
+// 	int total_numbers = 3;
+
+// 	float average = (number_1 + number_2 + number_3) / total_numbers;
+
+// 	printf("Here is the 3 Numbers of Average: %f", average);
+
+// 	return 0;
+// }
+
+// Question: Write a program to print the smallest number of two.
+
+// #include <stdio.h>
+
+// int main() {
+// 	int number_1, number_2, smallest;
+
+// 	printf("Enter first number: ");
+// 	scanf("%d", &number_1);
+
+// 	printf("Enter second number: ");
+// 	scanf("%d", &number_2);
+
+// 	if (number_1 < number_2) {
+// 		smallest = number_1;
+// 	} else {
+// 		smallest = number_2;
+// 	}
+
+// 	printf("The smallest number is: %d\n", smallest);
+
+// 	return 0;
+// }
+
+// Question: Write a program to check if given character is digit or not.
+
+// #include <stdio.h>
+
+// int main() {
+//     char ch;
+
+//     printf("Enter a character: ");
+//     scanf("%c", &ch);
+
+//     // Check if character is a digit
+//     if (ch >= '0' && ch <= '9') {
+//         printf("%c is a digit.\n", ch);
+//     } else {
+//         printf("%c is not a digit.\n", ch);
+//     }
+
+//     return 0;
+// }
