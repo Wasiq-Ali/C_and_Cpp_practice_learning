60ae05eb59a9199b53e6a02b47e934749c39adae
Wasiq-Ali <wasiqali878@gmail.com>
2026-04-06 01:09:56 +0500

    practice: Done chapter 01

--- Chapter 01/first.c
+++ Chapter 01/first.c
@@ -1,7 +1,22 @@
+// #include <stdio.h>
+
+// int main() {
+// 	printf("Hello World");
+
+// 	return 0;
+// }
+
 #include <stdio.h>
 
 int main() {
-	printf("Hello World");
+	int team_age;
+
+	printf("Hello Team \n");
+	printf("Tell me your age\n");
+
+	scanf("%d", &team_age);
+
+	printf("Here is my Team age: %d", team_age);
 
 	return 0;
 }
