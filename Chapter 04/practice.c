eb640dc6be37efa9c420f0969b51261f52868c47
Wasiq-Ali <wasiqali878@gmail.com>
2026-04-06 20:01:02 +0500

    practice: Done chapter 04

---
+++ Chapter 04/practice.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// int main() {
+// 	printf("Hello Chapter 6 \n");
+
+// 	for(int i = 1; i <=4; i++) {
+// 		for (int j = 1; j <= 5; j++) {
+// 			printf("*");
+// 		}
+// 		printf("\n");
+// 	}
+
+// 	return 0;
+// }
+
+// int main() {
+// 	printf("Hello Chapter 6 \n");
+
+// 	int init = 1;
+
+// 	while (init <= 10) {
+// 		printf("%d \n", init);
+
+// 		init++;
+// 	}
+
+// 	return 0;
+// }
+
+// int main() {
+// 	printf("Hello Chapter 6 \n");
+
+// 	int init = 1;
+
+// 	do {
+// 		printf("%d \n", init);
+
+// 		init++;
+// 	} while (init < 10);
+
+// 	return 0;
+// }
