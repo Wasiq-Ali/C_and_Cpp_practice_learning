aaad1c3a553290ac2d2dccf549ed1d0040d456e8
Wasiq-Ali <wasiqali878@gmail.com>
2026-04-12 00:49:27 +0500

    practice: Chapter 02 practice set is Done.

---
+++ Chapter 02/Practice-Set-02/practice.c
@@ -0,0 +1,48 @@
+// Question No 01: Which of the following is invalid in C?
+// 				a. int a=1; int b = a; True
+// 				b. int v = 3*3;	True
+// 				c. char dt = ‘21 dec 2020’ Wrong because char has only one char either it alphabet or alpha-numeric value.
+
+
+// Question No 02: What data type will 3.0/8 – 2 return?
+
+// #include <stdio.h>
+
+// int main() {
+// 	printf("%f \n", 3.0/8);
+
+// 	return 0;
+// }
+
+// Question No 03: Write a program to check whether a number is divisible by 97 or not.
+
+// #include <stdio.h>
+
+// int main(){
+// 	int input_number = 957392;
+
+// 	int checkpoint = (input_number % 97) == 0 ? 100 : 200; 
+
+// 	printf("%d \n", checkpoint);
+
+// 	return 0;
+// }
+
+// Question No 04: Explain step by step evaluation of 3*x/y – z+k, where x=2, y=3, z=3, k=1
+
+// #include <stdio.h>
+
+// int main(){
+// 	int x=2, y=10, z=3, k=1;
+
+// 	float calculate = 3*x/y - z+k;
+
+// 	printf("%f \n", calculate);
+
+// 	return 0;
+// }
+
+// Question No 05: 3.0 + 1 will be:
+// 				a. Integer.
+// 				b. Floating point number. This is the correct answer Int => Float => Float.
+// 				c. Character.
